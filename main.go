1903ad79a7c336069261dd0c17b7fbecdaaf6602
Markus Reschke <markus@markusreschke.name>
2017-05-28 12:33:18 +0200

    Use Cloudant Store instead of the in-memory one

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/cloudfoundry-community/go-cfenv"
 	"log"
 	"markusreschke.name/selfhostedsimplepolling/config"
 	"markusreschke.name/selfhostedsimplepolling/handlers"
@@ -8,7 +9,30 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"github.com/IBM-Bluemix/go-cloudant"
+	"errors"
 )
+
+func getCloudantCredentialsFromEnv(cloudantServiceName string) (user, password string, err error) {
+	appEnv, err := cfenv.Current()
+	if err != nil {
+		return "","",err
+	}
+	services := appEnv.Services
+	cloudantService, err := services.WithName(cloudantServiceName)
+	if err != nil {
+		return "","",err
+	}
+	user, isUserSet := cloudantService.CredentialString("username")
+	if !isUserSet {
+		return "","", errors.New("Cloudant Username not found in CF Env!")
+	}
+	password, isPasswordSet := cloudantService.CredentialString("password")
+	if !isPasswordSet {
+		return "","", errors.New("Cloudant Password not found in CF Env!")
+	}
+	return user, password, nil
+}
 
 func main() {
 	logger := log.New(os.Stdout, "logger: ", log.Lshortfile)
@@ -16,7 +40,18 @@ func main() {
 	if err != nil {
 		logger.Fatal("Error reading config file: ", err)
 	}
-	pollStore := poll.NewInMemoryStore()
+	cloudantUser, cloudantPassword, err := getCloudantCredentialsFromEnv("shsp-cloudant")
+	if err != nil {
+		logger.Fatalf("Couldn't fetch CLoudant credentials: %v", err)
+	}
+	cloudantClient, err := cloudant.NewClient(cloudantUser, cloudantPassword)
+	if err != nil {
+		logger.Fatalf("Couldn't connect to Cloudant: %v", err)
+	}
+	pollStore, err := poll.NewCloudantStore(cloudantClient, os.Getenv("CLOUDANT_DB"))
+	if err != nil {
+		logger.Fatalf("Couldn't create poll store: %v", err)
+	}
 	logger.Println(pollStore)
 	http.HandleFunc("/newpoll", handlers.GetNewPollRequestHandler(appConfig, logger, pollStore))
 	http.HandleFunc("/updatepoll", handlers.GetUpdatePollRequestHandler(appConfig, logger, pollStore))
