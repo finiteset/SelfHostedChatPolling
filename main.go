cf48a785df0059785064e06bbc5e1734045bab3a
Markus Reschke <markus@markusreschke.name>
2017-06-11 18:17:05 +0200

    Reenable cloudant store as default

--- main.go
+++ main.go
@@ -2,17 +2,17 @@ package main
 
 import (
 	"errors"
-	//"github.com/IBM-Bluemix/go-cloudant"
 	"github.com/cloudfoundry-community/go-cfenv"
 	"log"
 	"markusreschke.name/selfhostedsimplepolling/config"
 	"markusreschke.name/selfhostedsimplepolling/handlers"
 	"markusreschke.name/selfhostedsimplepolling/poll"
-	//"markusreschke.name/selfhostedsimplepolling/poll/cloudantstore"
-	"markusreschke.name/selfhostedsimplepolling/poll/memstore"
+	//"markusreschke.name/selfhostedsimplepolling/poll/memstore"
 	"net/http"
 	"os"
 	"strconv"
+	"github.com/IBM-Bluemix/go-cloudant"
+	"markusreschke.name/selfhostedsimplepolling/poll/cloudantstore"
 )
 
 func getCloudantCredentialsFromEnv(cloudantServiceName string) (user, password string, err error) {
@@ -42,7 +42,7 @@ func main() {
 	if err != nil {
 		logger.Fatal("Error reading config file: ", err)
 	}
-	/*cloudantUser, cloudantPassword, err := getCloudantCredentialsFromEnv("shsp-cloudant")
+	cloudantUser, cloudantPassword, err := getCloudantCredentialsFromEnv("shsp-cloudant")
 	if err != nil {
 		logger.Fatalf("Couldn't fetch CLoudant credentials: %v", err)
 	}
@@ -53,8 +53,8 @@ func main() {
 	pollStoreBackend, err := cloudantstore.NewCloudantStoreBackend(cloudantClient, os.Getenv("CLOUDANT_DB"))
 	if err != nil {
 		logger.Fatalf("Couldn't create poll store: %v", err)
-	}*/
-	pollStoreBackend := memstore.NewInMemoryStoreBackend()
+	}
+	//pollStoreBackend := memstore.NewInMemoryStoreBackend()
 	pollStore := poll.NewDefaultStore(pollStoreBackend)
 	http.HandleFunc("/newpoll", handlers.GetNewPollRequestHandler(appConfig, logger, pollStore))
 	http.HandleFunc("/updatepoll", handlers.GetUpdatePollRequestHandler(appConfig, logger, pollStore))
